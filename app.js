a914f9319a9c6c8ad98da982e41eef57a4b44c81
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-01-27 16:25:40 +0300

    Main | EJS, Public file, views file, partials file and upload _navigation

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express')
+const path = require('path');
 
 const app = express();
 
+//MIDDLEWARES
+app.use(express.static('public'));
+
+app.set("view engine", "ejs")
+
+// ROUTERS
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
-})
+   // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+   res.render('index')
+});
 
+app.get('/about', (req, res) => {
+    res.render('about')
+ });
 
-let port = 3333;
+ app.get('/contact', (req, res) => {
+    res.render('contact')
+ });
+
+ app.get('/post', (req, res) => {
+    res.render('post')
+ });
+
+let port = 4030;
 app.listen(port, () => {
     console.log(`🚀🚀🚀 Uygulama ${port} portunda aktif.`);
 })
