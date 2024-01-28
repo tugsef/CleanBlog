33b75916904b12e8bbdbd39316e6a226dda9e6b4
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-01-28 03:16:57 +0300

    Main | mongodb connect, ORM mongoode created, post created

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const express = require('express')
+const express = require('express');
+const ejs = require('ejs');
+
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
+
 const path = require('path');
+
+// Database
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 const app = express();
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.set("view engine", "ejs")
 
 // ROUTERS
-app.get('/', (req, res) => {
-   // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-   res.render('index')
+app.get("/",async (req, res) => {
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
 });
 
 app.get('/about', (req, res) => {
